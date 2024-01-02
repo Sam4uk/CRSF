57416486f1d291e28d1353df280f086ccc682b48
Sam4uk <sto.ukr.net@gmail.com>
2024-01-02 19:52:19 +0200

    PickChennel

---
+++ example/PickChennel/PickChennel.ino
@@ -0,0 +1,9 @@
+/**
+ *
+ * Using this sketch, you can see how your remote works.
+ *
+ * Connect the receiver to Serial3 and turn on the transmitter and receiver.
+ *
+ * If you use another chip with one hardware Serial, you can use a software one
+ *
+ */
