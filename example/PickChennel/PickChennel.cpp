7f68e3ad364eb372b08d354e1054fc9a5696f110
Sam4uk <sto.ukr.net@gmail.com>
2024-01-04 00:25:57 +0200

    четвер, 4 січня 2024 00:25:57 +0200

--- example/PickChennel/PickChennel.cpp
+++ example/PickChennel/PickChennel.cpp
@@ -3,44 +3,41 @@
  * @author Sam4uk
  * @version 0.1
  * @date 2024-01-02
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 #include <CrsfSerial.h>
 #if defined(__AVR_ATmega2560__)
-CrsfSerial                  
-    crsf(Serial3, CRSF_BAUDRATE); //  receiver connect to Serial3
+CrsfSerial crsf(Serial3, CRSF_BAUDRATE);
 #else
 #error NOT MEGA2560
 #endif
 
 /**
- * @brief The function that will be called upon receiving data via control channels
+ * @brief The function that will be called upon receiving data via control
+ * channels
  */
 void packetChannels();
 
 /**
  * @brief Executed once on power-up or reboot
  */
-void setup(){
-    Serial.begin(115200);
-    crsf.begin();
-    crsf.onPacketChannels = &packetChannels;
+void setup() {
+  Serial.begin(115200);
+  crsf.begin();
+  crsf.onPacketChannels = &packetChannels;
 }
 
 /**
  * @brief Executes cyclically while the power is on
  */
-void loop(){
-    crsf.loop();
-}
+void loop() { crsf.loop(); }
 
-void packetChannels(){
-    for (auto ch{1}; ch <= CRSF_NUM_CHANNELS; ++ch) {
+void packetChannels() {
+  for (auto ch{1}; ch <= CRSF_NUM_CHANNELS; ++ch) {
     Serial.print(crsf.getChannel(ch));
-    if (ch != CRSF_NUM_CHANNELS)
-        Serial.print(", ");
-    }
-    Serial.println();
+    if (ch != CRSF_NUM_CHANNELS) Serial.print(", ");
+  }
+  Serial.println();
 }
